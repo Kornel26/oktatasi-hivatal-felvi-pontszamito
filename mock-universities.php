f6f216b1d719714524baeb4640779644adaa3dee
Kornel26 <Kornel26@users.noreply.github.com>
2022-11-13 01:03:52 +0100

    Create point calculator class

---
+++ mock-universities.php
@@ -0,0 +1,22 @@
+<?php
+
+$universityData = [
+    [
+        'egyetem' => 'ELTE',
+        'kar' => 'IK',
+        'szak' => 'Programtervező informatikus',
+        'kotelezo' => 'matematika',
+        'kotelezo-szint' => 'közép',
+        'valaszthato' => ['biológia', 'fizika', 'informatika', 'kémia']
+    ],
+    [
+        'egyetem' => 'ELTE',
+        'kar' => 'BTK',
+        'szak' => 'Anglisztika',
+        'kotelezo' => 'angol',
+        'kotelezo-szint' => 'emelt',
+        'valaszthato' => ['francia', 'német', 'olasz', 'orosz', 'spanyol', 'történelem']
+    ]
+]
+
+?>
